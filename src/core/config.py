a1cdfc2e540d9edb50450fc721e8e7181b9f22fe
Konstantin Balandin <kostabalandin169@gmail.com>
2026-07-21 18:17:15 +0700

    add main file configuration

---
+++ src/core/config.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    LOG_LEVEL: str = "INFO"
+
+    LLM_BASE_URL: str
+    LLM_API_KEY: str
+    LLM_MODEL_PRIMARY: str
+    LLM_MODEL_CHEAP: str
+    OLLAMA_EMBED_MODEL: str
+    OLLAMA_BASE_URL: str
+    EMBED_DIMENSION: int = 768 
+
+settings = Settings()
